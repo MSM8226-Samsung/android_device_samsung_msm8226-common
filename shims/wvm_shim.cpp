9ae574f964d8bc706692b7341d641209f5e20ecd
PythonLimited <leonardomeitz@gmail.com>
2017-12-12 23:33:53 +0100

    linwvm: added new symbols (thanks Ivan again)

--- shims/wvm_shim.cpp
+++ shims/wvm_shim.cpp
@@ -14,8 +14,25 @@
  * limitations under the License.
  */
 
+#include <stdlib.h>
+#include <stdint.h>
+#include <media/IMediaSource.h>
+
 /* MediaBufferGroup::MediaBufferGroup */
 extern "C" int _ZN7android16MediaBufferGroupC1Ej(unsigned int);
 extern "C" int _ZN7android16MediaBufferGroupC1Ev() {
     return _ZN7android16MediaBufferGroupC1Ej(0);
+}
+
+extern "C" {
+    // getSeekTo(int64_t *time_us, SeekMode *mode)
+    void _ZNK7android12IMediaSource11ReadOptions9getSeekToEPxPNS1_8SeekModeE(int64_t*, int32_t*);
+    // bool getNonBlocking()
+    bool _ZNK7android12IMediaSource11ReadOptions14getNonBlockingEv();
+    void _ZNK7android11MediaSource11ReadOptions9getSeekToEPxPNS1_8SeekModeE(int64_t *time_us, int32_t *mode) {
+	_ZNK7android12IMediaSource11ReadOptions9getSeekToEPxPNS1_8SeekModeE(time_us, mode);
+    }
+    bool _ZNK7android11MediaSource11ReadOptions14getNonBlockingEv() {
+	return _ZNK7android12IMediaSource11ReadOptions14getNonBlockingEv();
+    }
 }
