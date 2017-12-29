4bb7629c4252b59580f63302be8eb02255a407ef
PythonLimited <leonardomeitz@gmail.com>
2017-12-29 10:59:26 +0100

    added new selinux rules for camera

--- shims/Android.mk
+++ shims/Android.mk
@@ -3,7 +3,7 @@ LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := wvm_shim.cpp
-LOCAL_SHARED_LIBRARIES := libstagefright_foundation
+LOCAL_SHARED_LIBRARIES := libstagefright_foundation liblog libmedia libcutils
 LOCAL_MODULE := libwvm_shim
 LOCAL_MODULE_TAGS := optional
 
