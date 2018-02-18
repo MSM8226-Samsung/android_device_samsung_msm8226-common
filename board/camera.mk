340a5fd9d5a3d0b08a3ffb753dc8361d33b8e68e
PythonLimited <leonardomeitz@gmail.com>
2018-02-18 21:12:52 +0100

    camera: we build that from source now!!!!!
    i and fragi ported the qcom camera HAL fro our neo ;)
    
    Change-Id: I32c48bb506263bc3dab45049c4461b4124661aef

--- board/camera.mk
+++ board/camera.mk
@@ -4,5 +4,4 @@ BOARD_GLOBAL_CFLAGS += -DCAMERA_VENDOR_L_COMPAT # Do we really need this???
 
 TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS := true
 TARGET_USE_VENDOR_CAMERA_EXT := true # Dont know what this does but everyone else uses it :)
-USE_DEVICE_SPECIFIC_CAMERA := true
 TARGET_HAS_LEGACY_CAMERA_HAL1 := true
